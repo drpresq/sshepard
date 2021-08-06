c2c23892ed5a5d1579d69395c59c8faec2518708
George <drpresq@gmail.com>
2021-08-06 19:36:20 -0400

    Updated setup.py classifiers

--- setup.py
+++ setup.py
@@ -17,5 +17,16 @@ setup(
     url="https://github.com/drpresq/sshepherd",
     keywords="",
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Utilities'
     ],
 )
